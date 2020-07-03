0ef63808fc928198c3917290d931c1ec248f003a
Eduardo Kuchenbecker <eduardokuchenbecker@hotmail.com>
2020-07-02 21:56:33 -0300

    corrigir requiisição de login

--- servidor.js
+++ servidor.js
@@ -9,9 +9,11 @@ app.use(cors());
 app.use(express.json());
 
 app.post("/api/users/login", async (req, res, next) => {
+  console.log("chegou no backend");
+  
   const user = await index.login({
-    username: req.body.email,
-    password: req.body.password,
+    username: req.body.values.username,
+    password: req.body.values.password,
   });
   if (user) {
     res.json({ user });
